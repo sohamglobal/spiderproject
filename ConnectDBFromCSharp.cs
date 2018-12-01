cfd20ce3ae3d4d570d78833c3532adcdb361c663
EthanHunt <praffull@outlook.com>
2018-12-01 10:23:24 +0530

    jdbc modified and ado.net added

---
+++ ConnectDBFromCSharp.cs
@@ -0,0 +1,5 @@
+//C# program to connect to Microsoft SQL Server 2017 DB with ADO.NET
+
+using System;
+using System.Data;
+using System.Data.SqlClient;
--- JDBCWithMySQL.java
+++ JDBCWithMySQL.java
@@ -1,1 +1,40 @@
 //JDBC program to connect and communicate with MySQL 8 Database from Java 11
+
+import java.sql.*;
+
+class JDBCWithMySQL
+{
+public static void main(String args[])
+{
+Connection con;
+Statement st;
+ResultSet rs;
+
+try
+{
+Class.forName("com.mysql.cj.jdbc.Driver");
+
+con=DriverManager.getConnection("jdbc:mysql://localhost:3306/visadb?user=root&password=crosspolo&useSSL=false&allowPublicKeyRetrieval=true");
+
+st=con.createStatement();
+rs=st.executeQuery("select accnm,balance from accounts;");
+
+while(rs.next())
+{
+System.out.print(rs.getString("accnm")+"\t");
+System.out.println(rs.getDouble(2));
+}
+
+con.close();
+}
+catch(Exception e)
+{
+System.out.println(e);
+}
+
+}
+}
+
+
+
+
